26223307007ee916855c62abdee84f7b4770d1d6
javad <javadMohammadi.js@gmail.com>
2023-01-06 21:07:35 -0800

    Accordion userProfile

--- src/components/components/Userprofile/utils/CardClass.jsx
+++ src/components/components/Userprofile/utils/CardClass.jsx
@@ -1,51 +1,64 @@
-import React from 'react'
-import style from '../style/userpage.module.css'
-import TitleClass from './TitleClass'
-import teacherTestPhoto from '../../../../assets/images/users/avatar-6.jpg'
-import { FiMoreVertical } from 'react-icons/fi'
+import React from "react";
+import style from "../style/userpage.module.css";
+import TitleClass from "./TitleClass";
+import teacherTestPhoto from "../../../../assets/images/users/avatar-6.jpg";
+import { FiMoreVertical } from "react-icons/fi";
+import Accordion from "react-bootstrap/Accordion";
 function CardClass() {
   return (
     <div>
-      <TitleClass />
-      <div className={`${style.classListContainerRow} mt-2 row`}>
-        <div className="col-12 col-lg-6">
-          <div className="d-flex align-items-center justify-content-center justify-content-lg-start ">
-            <div className={`${style.teacherCardPhoto} mx-2 `}>
-              <img src={teacherTestPhoto} alt="teacher name" />
-            </div>
-            <div className="d-flex flex-column justify-content-around align-items-center">
-              <div className={`${style.containerClassInfo} my-2`}>
-                <span> کلاس خصوصی راشین احمدی </span>
-                <span className={`${style.flagClass} mx-2`}>کلاس خصوصی</span>
+      <Accordion defaultActiveKey="1">
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>
+            <TitleClass />
+          </Accordion.Header>
+          <Accordion.Body>
+            <div className={`${style.classListContainerRow} mt-2 row`}>
+              <div className="col-12 col-lg-6">
+                <div className="d-flex align-items-center justify-content-center justify-content-lg-start ">
+                  <div className={`${style.teacherCardPhoto} mx-2 `}>
+                    <img src={teacherTestPhoto} alt="teacher name" />
+                  </div>
+                  <div className="d-flex flex-column justify-content-around align-items-center">
+                    <div className={`${style.containerClassInfo} my-2`}>
+                      <span> کلاس خصوصی راشین احمدی </span>
+                      <span className={`${style.flagClass} mx-2`}>
+                        کلاس خصوصی
+                      </span>
+                    </div>
+                    <div className={`${style.containerClassInfo} my-2`}>
+                      <span>
+                        تعدا کل جلسات : <b className={style.numberClass}> 1 </b>
+                      </span>
+                      <span className={`mx-3`}>
+                        تعداد جلسات زمانبندی نشده :
+                        <b className={style.numberClass}> 9 </b>
+                      </span>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className={`${style.containerClassInfo} my-2`}>
-                <span>
-                  تعدا کل جلسات : <b className={style.numberClass}> 1 </b>
-                </span>
-                <span className={`mx-3`}>
-                  تعداد جلسات زمانبندی نشده :
-                  <b className={style.numberClass}> 9 </b>
-                </span>
+              <div className="col-12 col-lg-6 d-flex align-items-center justify-content-center justify-content-lg-end mt-3">
+                <div className={`mx-2 ${style.btnActionCardClass}`}>
+                  <button className="border-0 p-2 px-3 rounded-2">
+                    چت با استاد
+                  </button>
+                </div>
+                <div className={`mx-2 ${style.btnActionCardClass}`}>
+                  <button className="border-0 p-2 px-3 rounded-2">
+                    زمانبندی کلاس
+                  </button>
+                </div>
+                <div className={`mx-2 curoser`}>
+                  <FiMoreVertical size={20} />
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-        <div className="col-12 col-lg-6 d-flex align-items-center justify-content-center justify-content-lg-end mt-3">
-          <div className={`mx-2 ${style.btnActionCardClass}`}>
-            <button className="border-0 p-2 px-3 rounded-2">چت با استاد</button>
-          </div>
-          <div className={`mx-2 ${style.btnActionCardClass}`}>
-            <button className="border-0 p-2 px-3 rounded-2">
-              زمانبندی کلاس
-            </button>
-          </div>
-          <div className={`mx-2 curoser`}>
-            <FiMoreVertical size={20} />
-          </div>
-        </div>
-      </div>
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
     </div>
-  )
+  );
 }
 
-export default CardClass
+export default CardClass;
--- src/components/components/Userprofile/utils/TitleClass.jsx
+++ src/components/components/Userprofile/utils/TitleClass.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import style from '../style/userpage.module.css'
+import React from "react";
+import style from "../style/userpage.module.css";
+import { BsChevronDown } from "react-icons/bs";
 function TitleClass() {
   return (
     <div
-      className={`${style.titleClass} d-flex justify-content-between align-items-center border-bottom border-gray`}
+      className={`${style.titleClass} d-flex justify-content-between align-items-center border-bottom border-gray w-100`}
     >
       <div className="mb-3">
         <b>کلاس های پیش رو</b>
+        <b className={`mx-3`}>
+          <BsChevronDown className="BsChevronDown" size={18} />
+        </b>
       </div>
       <div>
         <b className={`${style.titleClassShow} curoser mb-0`}>مشاهده همه</b>
       </div>
     </div>
-  )
+  );
 }
 
-export default TitleClass
+export default TitleClass;

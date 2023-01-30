10a99c66fdb7677c27ec849762eb9e00393e4de5
javad <javadMohammadi.js@gmail.com>
2023-01-30 20:47:57 +0330

    decription teacher

--- src/components/components/Teacher/TeacherDetail/TeacherDetailInfo.jsx
+++ src/components/components/Teacher/TeacherDetail/TeacherDetailInfo.jsx
@@ -14,7 +14,12 @@ function TeacherDetailInfo() {
   const { id } = params
   const teachers = useSelector(selectAllTeacher)
   const findteacher = teachers.find((teacher) => teacher.id === id)
-  const { photoTeacher, nameTeacher } = findteacher
+  const {
+    photoTeacher,
+    nameTeacher,
+    discriptionTeacher,
+    discriptionTeacherEnglish,
+  } = findteacher
   useEffect(() => {
     console.log(findteacher, 111)
   }, [])
@@ -23,53 +28,71 @@ function TeacherDetailInfo() {
       <div className={style.bgCoverTeacher}>
         <img src={cover1} alt="" />
       </div>
-      <div className={` w-100 d-flex justify-content-between`}>
-        <div className="d-flex">
-          <div className={style.teacherImgContaienr}>
-            <img
-              className={`  ${style.teacherImg}`}
-              src={photoTeacher}
-              alt=""
-            />
-          </div>
-          <div className={`m-2`}>
-            <div className="d-flex align-items-center my-2 mt-0">
-              <p className="m-0 mx-2"> {nameTeacher} </p>
-              <img width="22" className="rounded-1" src={us} alt="" />
+      <div className="row justify-content-between">
+        <div className="col-12 col-md-7 d-flex flex-column">
+          <div className="d-flex">
+            {/* teacher picture */}
+            <div className={style.teacherImgContaienr}>
+              <img
+                className={`  ${style.teacherImg}`}
+                src={photoTeacher}
+                alt=""
+              />
             </div>
-            <div className={`d-flex`}>
-              <div className={`mx-1 d-flex`}>
-                <AiFillStar size={19} className={``} />
-                <AiFillStar size={19} className={`mx-1`} />
-                <AiFillStar size={19} className={`mx-1`} />
-                <AiFillStar size={19} className={`mx-1`} />
-                <AiFillStar size={19} className={`mx-1`} />
+            {/* basic information about teacher like skills all students has */}
+            <div className={`m-2`}>
+              <div className="d-flex align-items-center my-2 mt-0">
+                <p className="m-0 mx-2"> {nameTeacher} </p>
+                <img width="22" className="rounded-1" src={us} alt="" />
               </div>
-              <span>
-                <span className="mx-1">4.9</span>
+              <div className={`d-flex`}>
+                <div className={`mx-1 d-flex`}>
+                  <AiFillStar size={19} className={``} />
+                  <AiFillStar size={19} className={`mx-1`} />
+                  <AiFillStar size={19} className={`mx-1`} />
+                  <AiFillStar size={19} className={`mx-1`} />
+                  <AiFillStar size={19} className={`mx-1`} />
+                </div>
                 <span>
-                  ( از
-                  <span className="">177</span>
-                  نظر)
+                  <span className="mx-1">4.9</span>
+                  <span>
+                    ( از
+                    <span className="">177</span>
+                    نظر)
+                  </span>
                 </span>
-              </span>
+              </div>
+              <div className="mt-2">
+                <AiFillEuroCircle size={20} />
+                <span className="mx-2">زبان انگلیسی استاد</span>
+              </div>
+              <div className="mt-2">
+                <AiFillEuroCircle size={20} />
+                <span className="mx-2">زبان انگلیسی استاد</span>
+              </div>
+              <div className="mt-2">
+                <AiFillEuroCircle size={20} />
+                <span className="mx-2">زبان انگلیسی استاد</span>
+              </div>
             </div>
-            <div className="mt-2">
-              <AiFillEuroCircle size={20} />
-              <span className="mx-2">زبان انگلیسی استاد</span>
-            </div>
-            <div className="mt-2">
-              <AiFillEuroCircle size={20} />
-              <span className="mx-2">زبان انگلیسی استاد</span>
-            </div>
-            <div className="mt-2">
-              <AiFillEuroCircle size={20} />
-              <span className="mx-2">زبان انگلیسی استاد</span>
-            </div>
+          </div>
+          <div>
+            <p className="text">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Doloribus voluptatibus assumenda ut, consequuntur earum itaque
+              perspiciatis rem fuga necessitatibus a quaerat voluptatem laborum
+              tempora facere culpa nostrum voluptate magnam non.
+            </p>
+            <p className="text-right">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga
+              ipsa quod aliquam laborum repellat! Iure amet magni eos eaque,
+              odit vel ratione. Aliquid animi ducimus facilis, tempore molestiae
+              blanditiis culpa.
+            </p>
           </div>
         </div>
         <section
-          className={`${style.containerVedioFixed} shadow d-none d-md-flex`}
+          className={`${style.containerVedioFixed} shadow d-none d-md-flex col-5 col-lg-3 position-sticky top-0 mt-3 mx-3`}
         >
           <div className={style.fixed}>
             <Vedio
@@ -92,15 +115,9 @@ function TeacherDetailInfo() {
           </div>
         </section>
       </div>
-      <div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
     </div>
   )
 }
 
 export default TeacherDetailInfo
+//linear-gradient(0deg,hsla(0,0%,100%,.61961),transparent)

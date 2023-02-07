93e6eaea8a36ba07e6b02d6a043e65f689e99ed5
javad <javadMohammadi.js@gmail.com>
2023-02-07 23:49:41 +0330

    header modal

--- src/components/components/Teacher/TeacherDetail/components/reserved-modal/ReservedModalHasTime.jsx
+++ src/components/components/Teacher/TeacherDetail/components/reserved-modal/ReservedModalHasTime.jsx
@@ -3,11 +3,12 @@ import style from './styles/ReservedModal.module.css'
 import { AiOutlineClose } from 'react-icons/ai'
 import { useDispatch } from 'react-redux'
 import { showModalSetClassTimeHandler } from '../../../../../../app/store/Teacher store/Teacher'
+import HeaderModal from './components/HeaderModal'
 function ReservedModal({ findteacher }) {
   const dispatch = useDispatch()
   return (
     <div className={`${style.containerModal}`}>
-      <div className={style.content}>
+      <div className={`${style.content} container`}>
         <div className="mt-4 mx-4">
           <span
             className="p-1 curoser"
@@ -16,7 +17,9 @@ function ReservedModal({ findteacher }) {
             <AiOutlineClose size={18} />
           </span>
         </div>
-        <div className="mt-3 px-4">d</div>
+        <div className="mt-3 px-4">
+          <HeaderModal />
+        </div>
       </div>
     </div>
   )
--- src/components/components/Teacher/TeacherDetail/components/reserved-modal/components/HeaderModal.jsx
+++ src/components/components/Teacher/TeacherDetail/components/reserved-modal/components/HeaderModal.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
-
+import style from '../styles/ReservedModal.module.css'
 function HeaderModal() {
-  return <div>HeaderModal</div>
+  return (
+    <div className="d-flex w-100 justify-content-between  align-items-center row">
+      <div className={`${style.headerTitleBorder} col-5`}>
+        <div className={`${style.circle}`}></div>
+        <div className={`mt-2 ${style.borderLeft} position-relative fontSize`}>
+          نوع کلاس
+        </div>
+      </div>
+      <div className={`${style.headerTitleBorder} col-5`}>
+        <div className={`${style.circle}  `}></div>
+        <div
+          className={`mt-2 ${style.borderLeft} position-relative fontSize ${style.secondBorderLeft}`}
+        >
+          تعیین زمان بندی
+        </div>
+      </div>
+      <div className=" col-1 text-start">
+        <div className={`${style.circle} ms-0`}></div>
+        <div className={`mt-2 fontSize`}>پرداخت</div>
+      </div>
+    </div>
+  )
 }
 
 export default HeaderModal
